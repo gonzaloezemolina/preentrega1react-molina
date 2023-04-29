cabc84b67d74692baee242f62cda933991f2f6e0
Gonzalo <gonzaloezemolina@gmail.com>
2023-04-29 19:16:58 -0300

    solucion deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import NavBar from './components/NavBar/NavBar'
+
 
 function App() {
   
